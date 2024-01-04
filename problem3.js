b85084af2df2344036daca4997c3662da760906b
nayon117 <hasibul.nayon1@gmail.com>
2024-01-04 17:15:39 +0600

    solved inch to feet problem

---
+++ problem3.js
@@ -0,0 +1,10 @@
+// convert inch to feet
+const inchToFeet = (inch) => {
+    const result = inch / 12;
+    return result;
+}
+const inch = 144;
+const feet = inchToFeet(inch)
+// console.log(feet);
+
+// convert miles to kilometer 
