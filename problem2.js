3b0585fbbe81c8877d2aadf3f440e1581564d9f6
nayon117 <hasibul.nayon1@gmail.com>
2024-01-04 17:07:45 +0600

    solved break-continue problem

---
+++ problem2.js
@@ -0,0 +1,9 @@
+// show all the even number between 412 to 496
+for (let i = 412; i <= 496; i+=2){
+    // console.log(i);
+}
+
+// show all the odd number between 101 to 123
+for (let i = 101; i <= 123; i += 2){
+    // console.log(i);
+}
