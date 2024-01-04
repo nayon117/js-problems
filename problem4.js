042218985e09a368ca90673e3524aca84e2ae2d7
nayon117 <hasibul.nayon1@gmail.com>
2024-01-04 18:38:15 +0600

    find even and odd number of an array

--- problem4.js
+++ problem4.js
@@ -10,3 +10,27 @@ const getSum = (numbers) => {
 const arr = [3, 4, 4, 5, 6]
 const sum = getSum(arr)
 // console.log(sum);
+
+// find odd numbers sum;
+const getOddnumbers = (numbers) => {
+    const oddNumber = []
+    for (let i = 0; i < numbers.length; i++){
+        const element = numbers[i]
+        if (element % 2 !== 0) {
+            oddNumber.push(element)
+        }
+    }
+    return oddNumber;
+}
+
+// even number
+const findEven = (numbers) => {
+    const evenNumber = []
+    for (let i = 0; i < numbers.length; i++){
+        const element = numbers[i]
+        if (element % 2 === 0) {
+            evenNumber.push(element)
+        }
+    }
+    return evenNumber;
+}
