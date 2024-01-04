b41af32affcdac27b3a42971b59ff106bdc77dca
nayon117 <hasibul.nayon1@gmail.com>
2024-01-04 17:19:07 +0600

    solved miles to kilo problem

--- problem3.js
+++ problem3.js
@@ -7,4 +7,12 @@ const inch = 144;
 const feet = inchToFeet(inch)
 // console.log(feet);
 
-// convert miles to kilometer 
+// convert miles to kilometer
+const milesToKilometer = (miles) => {
+    const result = miles * 1.6
+    return result;
+}
+
+const miles = 12;
+const kilometer = milesToKilometer(miles)
+// console.log(kilometer);
