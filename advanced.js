9aa7b69b9dcb5060e2a5957ca4e18361cf626177
nayon117 <hasibul.nayon1@gmail.com>
2024-06-17 16:27:27 +0600

    solve debounce problem

--- advanced.js
+++ advanced.js
@@ -3,3 +3,15 @@
 const findIntersection = (arr1,arr2) =>{
     return arr1.map(element=>arr2.includes(element))
 }
+
+// Write a function that debounces another function by a specified delay.
+function debounce(func, delay) {
+    let timeoutId;
+    return function(...args) {
+      if (timeoutId) clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        func.apply(this, args);
+      }, delay);
+    };
+  }
+  
