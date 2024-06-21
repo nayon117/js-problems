61fa46c14ceaafd4d5255d2678fb1598b5c550c8
nayon117 <hasibul.nayon1@gmail.com>
2024-06-21 11:15:57 +0600

    merged two array

--- easy/problemset3.js
+++ easy/problemset3.js
@@ -9,4 +9,21 @@ function countVowels(str) {
     }
     return count;
   }
+
+  // Merge Two Sorted Arrays
+  function mergeSortedArrays(arr1, arr2) {
+    let merged = [];
+    let i = 0, j = 0;
+    while (i < arr1.length && j < arr2.length) {
+      if (arr1[i] < arr2[j]) {
+        merged.push(arr1[i]);
+        i++;
+      } else {
+        merged.push(arr2[j]);
+        j++;
+      }
+    }
+    return merged.concat(arr1.slice(i)).concat(arr2.slice(j));
+  }
+  
   
