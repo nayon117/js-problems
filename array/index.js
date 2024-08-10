69f20de1008daf60be9f483fe6395a007132853e
nayon117 <hasibul.nayon1@gmail.com>
2024-08-10 06:35:34 +0600

    solve sumarray problem

--- array/index.js
+++ array/index.js
@@ -6,3 +6,15 @@ const reverseArray = (arr) =>{
     }
     return reverseArr;
 }
+
+// sum of all items
+const sumArray = (arr) =>{
+    let sum = 0;
+    for(let i=0; i<arr.length; i++){
+        sum += arr[i]
+    }
+    return sum;
+}
+
+const arr = [1,2,3,4,5,6]
+// console.log(sumArray(arr))
