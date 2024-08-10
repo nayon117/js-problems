7ab42337a829668b7b885f0502cdea6d0e7ca880
nayon117 <hasibul.nayon1@gmail.com>
2024-08-10 07:42:44 +0600

    solve all advanced array problems

--- array/index.js
+++ array/index.js
@@ -56,3 +56,56 @@ const findMissingNumber = (arr) =>{
     return expectedSum - actualSum
 }
 // console.log(findMissingNumber([0, 1, 3, 4, 5])); // Output: 2
+
+/*
+Problem 3: Remove Duplicates from Sorted Array
+
+Problem Statement:
+Given a sorted array, remove the duplicates in place such that each element appears only once and returns the new length. */
+
+const removeDuplicates = (arr) =>{
+    let uniqueIndex = 0;
+    for(let i=1; i<arr.length; i++){
+        if(arr[i] !== arr[uniqueIndex]){
+            uniqueIndex++
+            arr[uniqueIndex] = arr[i]
+        }
+    }
+    return uniqueIndex+1
+}
+let nums = [1, 1, 2, 2, 3, 4, 4];
+// console.log(removeDuplicates(nums));
+
+/*
+Problem 4: Move Zeroes
+Write a function moveZeroes that takes an array and moves all zeroes to the end, maintaining the order of the other elements. */
+
+const moveZeroes = (arr) =>{
+    let nonZeroIndex = 0;
+    for(let i =0 ; i<arr.length; i++){
+        if(arr[i] !== 0){
+            [arr[nonZeroIndex],[arr[i]]] = [arr[i], [arr[nonZeroIndex]]]
+            nonZeroIndex++
+        }
+    }
+    return arr;
+}
+// console.log(moveZeroes([0, 1, 0, 3, 12]));
+
+/*
+Problem 5: Intersection of Two Arrays
+Write a function intersect that takes two arrays and returns their intersection.*/
+
+const intersect = (arr1,arr2) => {
+    let set1 = new Set(arr1)
+    let intersection = new Set()
+
+    for (let num of arr2) {
+        if(set1.has(num)){
+            intersection.add(num)
+        }
+    }
+    return intersection
+}
+
+console.log(intersect([1, 2, 2, 1], [2, 2])); // Output: [2, 2]
