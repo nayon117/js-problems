9c8c67bbc4a311295e68181eb80cc8873895ba54
nayon117 <hasibul.nayon1@gmail.com>
2024-08-10 06:58:21 +0600

    solve missing num problem

--- array/index.js
+++ array/index.js
@@ -1,3 +1,4 @@
+// Day 1: introduction of Array
 // Reverse an array
 const reverseArray = (arr) =>{
     let reverseArr = []
@@ -18,3 +19,40 @@ const sumArray = (arr) =>{
 
 const arr = [1,2,3,4,5,6]
 // console.log(sumArray(arr))
+
+// Find the maximum
+const findLargest = (arr) =>{
+    let largestArr = arr[0]
+    for(let i=1; i<arr.length; i++){
+        if(arr[i] > largestArr){
+            largestArr=arr[i]
+        }
+    }
+    return largestArr;
+}
+const arr1 = [1,2,3,4,5,6]
+// console.log(findLargest(arr))
+
+// Day 2: Advanced array problems
+// Problem 1: Rotate an Array
+const rotateArray = (arr,k) =>{
+    k = k % arr.length
+    let endPart = arr.splice(-k)
+    arr.unshift(...endPart)
+    return arr;
+}
+// console.log(rotateArray([1, 2, 3, 4, 5,6,7], 3)); // Output: [4, 5, 1, 2, 3]
+
+/*
+Problem 2: Find the Missing Number in an Array
+
+Problem Statement:
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array. */
+
+const findMissingNumber = (arr) =>{
+    let n = arr.length;
+    let expectedSum = n*(n+1)/2
+    let actualSum = arr.reduce((sum,num)=>(sum+num),0)
+    return expectedSum - actualSum
+}
+// console.log(findMissingNumber([0, 1, 3, 4, 5])); // Output: 2
