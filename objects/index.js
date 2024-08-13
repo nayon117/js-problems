9cd0d241e1a93d4057398b521d8349dedf390ed6
nayon117 <hasibul.nayon1@gmail.com>
2024-08-13 18:09:31 +0600

    solve nested object problem

--- objects/index.js
+++ objects/index.js
@@ -14,4 +14,27 @@ function createPerson(name, age, country) {
 
 let person = createPerson("Alice", 25, "Canada");
 person.greet(); // Output: Hello, my name is Alice.
-console.log(person.greet())
+// console.log(person.greet())
+
+// Problem 2: Nested Objects
+
+function createStudent(name, age, marks) {
+   let student = {
+    name : name,
+    age:age,
+    marks:marks,
+    getAverageMarks:function(){
+      let total = 0;
+      let subjectCount = 0
+      for(let subject in this.marks) {
+        total += this.marks[subject]
+        subjectCount ++
+      }
+      return total /subjectCount
+    }
+   }
+   return student;
+}
+
+let student = createStudent("Bob", 20, { math: 80, english: 75, science: 85 });
+console.log(student.getAverageMarks()); // Output: 80
