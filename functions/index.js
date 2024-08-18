240c4c516b4cead9b4c958a1a74f891e01945c9e
nayon117 <hasibul.nayon1@gmail.com>
2024-08-18 19:12:28 +0600

    solve findmax problem

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,22 @@ function factorial(n) {
    }else{
         return n * factorial(n-1);
    }
-   
+
 }
 
-console.log(factorial(5)); // Output: 120
+// console.log(factorial(5)); // Output: 120
+
+// Problem 2: Find the Largest Number in an Array
+
+function findMax(arr) {
+   let max = arr[0]
+   for(let i=1; i<arr.length; i++){
+    if(arr[i] > max){
+        max = arr[i]
+    }
+   }
+   return max;
+}
+
+console.log(findMax([1, 3, 7, 2, 5])); // Output: 7
+
