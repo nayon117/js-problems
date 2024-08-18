3097f9980b497f14009ebd19cf9b317231be28e9
nayon117 <hasibul.nayon1@gmail.com>
2024-08-18 19:10:04 +0600

    solve factorial problem

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+// Problem 1: Calculate Factorial
+
+function factorial(n) {
+   if(n===0 || n===1) {
+     return 1;
+   }else{
+        return n * factorial(n-1);
+   }
+   
+}
+
+console.log(factorial(5)); // Output: 120
