3e82a8ed6d371d0258b7186802d623da4500f660
nayon117 <hasibul.nayon1@gmail.com>
2024-08-18 19:17:09 +0600

    solve function problem

--- functions/index.js
+++ functions/index.js
@@ -23,5 +23,40 @@ function findMax(arr) {
    return max;
 }
 
-console.log(findMax([1, 3, 7, 2, 5])); // Output: 7
+// console.log(findMax([1, 3, 7, 2, 5])); // Output: 7
+
+// Problem 3: Check if a String is a Palindrome
+
+function isPalindrome(str) {
+    let reverseStr = str.split('').reverse().join('')
+    return reverseStr ===str
+}
+
+// console.log(isPalindrome("racecar")); // Output: true
+// console.log(isPalindrome("hello")); // Output: false
+
+// Problem 4: Convert Fahrenheit to Celsius
+
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9
+}
+
+// console.log(toCelsius(68)); // Output: 20
+
+// Problem 5: Sum of All Even Numbers in an Array
+
+
+function sumEvenNumbers(arr) {
+    let sum = 0;
+    for(let i = 0 ; i<arr.length; i++){
+        if(arr[i] %2===0){
+            sum += arr[i]
+        }
+    }
+    return sum;
+}
+
+console.log(sumEvenNumbers([1, 2, 3, 4, 5])); // Output: 6
+
+
 
