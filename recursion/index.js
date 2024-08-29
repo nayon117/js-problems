22c8088f44f1d4c4798fd4fa74a3f601a6dfc06b
nayon117 <hasibul.nayon1@gmail.com>
2024-08-29 21:20:10 +0600

    fibonacci using recursion

--- recursion/index.js
+++ recursion/index.js
@@ -5,3 +5,12 @@ function factorial(n) {
         return n * factorial(n - 1);
     }
 }
+
+
+function fibonacci(n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
