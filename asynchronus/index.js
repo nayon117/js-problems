a08c36e861759fb514602d262f06f3d1724b2796
nayon117 <hasibul.nayon1@gmail.com>
2024-09-09 19:52:27 +0600

    handle asynchronus task

--- asynchronus/index.js
+++ asynchronus/index.js
@@ -9,3 +9,14 @@ async function fetchDataFromAPI() {
         console.error("Error fetching data", error);
     }
 }
+
+
+function performAsyncTasks() {
+    const task1 = new Promise((resolve) => setTimeout(() => resolve("Task 1 completed!"), 1000));
+    const task2 = new Promise((resolve) => setTimeout(() => resolve("Task 2 completed!"), 2000));
+    const task3 = new Promise((resolve) => setTimeout(() => resolve("Task 3 completed!"), 3000));
+
+    Promise.all([task1, task2, task3]).then((results) => {
+        results.forEach((result) => console.log(result));
+    });
+}
